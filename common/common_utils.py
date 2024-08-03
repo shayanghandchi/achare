3d9c1c8c873da57cc5c3539952cbe633338cca47
ShayanGhandchi <87217508+shayanghandchi@users.noreply.github.com>
2024-08-04 03:02:55 +0330

    add some utils for sanitize user data

---
+++ common/common_utils.py
@@ -0,0 +1,36 @@
+def persian_digit_to_english(num: str):
+    if num is None:
+        return None
+    persian_digits = "۰۱۲۳۴۵۶۷۸۹"
+    for i in range(10):
+        num = num.replace(persian_digits[i], str(i))
+    return num
+
+
+def arabic_to_persian(s: str):
+    if s is None:
+        return None
+    chars = {
+        "ك": "ک",
+        "دِ": "د",
+        "بِ": "ب",
+        "زِ": "ز",
+        "ذِ": "ذ",
+        "شِ": "ش",
+        "سِ": "س",
+        "ى": "ی",
+        "ي": "ی",
+        "١": "۱",
+        "٢": "۲",
+        "٣": "۳",
+        "٤": "۴",
+        "٥": "۵",
+        "٦": "۶",
+        "٧": "۷",
+        "٨": "۸",
+        "٩": "۹",
+        "٠": "۰",
+    }
+    for ar, pr in chars.items():
+        s = s.replace(ar, pr)
+    return s
