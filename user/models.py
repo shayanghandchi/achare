646c9cd8efc113096a7d15307944d2a104997b01
ShayanGhandchi <87217508+shayanghandchi@users.noreply.github.com>
2024-08-04 03:19:27 +0330

    adding user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from common.common_utils import arabic_to_persian, persian_digit_to_english
 
-# Create your models here.
+
+class User(models.Model):
+    phone_number = models.CharField(
+        max_length=11,
+        unique=True,
+        verbose_name="شماره همراه",
+        db_index=True,
+        validators=[
+            RegexValidator(regex="^0[0-9]{10}$", message="فرمت شماره همراه صحیح نیست.")
+        ],
+    )
+    first_name = models.CharField(max_length=100, null=True, verbose_name="نام")
+    last_name = models.CharField(max_length=100, null=True, verbose_name="نام خانوادگی")
+    email = models.EmailField(
+        validators=[
+            RegexValidator(
+                regex=r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+                message="فرمت ایمیل معتبر نیست.",
+            )
+        ],
+        unique=True,
+        blank=False,
+        null=False,
+        verbose_name="ایمیل",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.phone_number = persian_digit_to_english(self.phone_number)
+        self.first_name = arabic_to_persian(self.first_name)
+        self.last_name = arabic_to_persian(self.last_name)
+        super().save(*args, **kwargs)
+
+    def fullname(self):
+        return f"{self.first_name} {self.last_name}"
